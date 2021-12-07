68ed7d36a764ae10ebd39e05fee86b2b104e6afa
Jerry <82269916@qq.com>
2021-12-07 11:56:27 +0800

    Rename files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,18 +2,18 @@
 
 make
 
-docker build -t jerry9916/csi-demo-driver:latest .
+docker build -t jerry9916/demo-csi-driver:latest .
 
 kubectl delete -f deploy/nginx.yaml
 kubectl delete -f deploy/pvc.yaml
 kubectl delete -f deploy/storageclass.yaml
-kubectl delete -f deploy/csi-demo-driver.yaml
+kubectl delete -f deploy/demo-csi-driver.yaml
 kubectl delete -f deploy/rbac.yaml
 
 sleep 15
 
 kubectl apply -f deploy/rbac.yaml
-kubectl apply -f deploy/csi-demo-driver.yaml
+kubectl apply -f deploy/demo-csi-driver.yaml
 kubectl apply -f deploy/storageclass.yaml
 kubectl apply -f deploy/pvc.yaml
 kubectl apply -f deploy/nginx.yaml
